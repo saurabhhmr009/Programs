07c8534b5421794fccf90c7c13569e90be5e6d35
Saurabh Bhamra <saurabh.hmr009@gmail.com>
2021-02-16 20:55:19 +0530

    Deleted the extra files

--- C/int.c
+++
@@ -1,9 +0,0 @@
-// Max number represented in C by int is 2147483647, after that it prints the result in -ve
-
-#include<stdio.h>
-int main(void) {
-	int num = 0;
-	num = 2147483647;
-	printf("%d\n", num);
-	return 0;
-}
--- C/start.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-
-int main(void) {
-	int i = 1;
-        printf("%d %d %d\n", ++i, i++, i);
-	return 0;
-}
