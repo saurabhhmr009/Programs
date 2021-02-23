c4a7d0e9ff9f719dae1a67c7c2edfe9203f90b78
Saurabh Bhamra <saurabh.hmr009@gmail.com>
2021-02-23 13:36:43 +0530

    Various string validator functions in Python

---
+++ python/string_validators.py
@@ -0,0 +1,9 @@
+#Program to show usage of different string validator fucntions in Python
+
+if __name__ == '__main__':
+    s = input()
+    print(any([char.isalnum() for char in s]))
+    print(any([char.isalpha() for char in s]))
+    print(any([char.isdigit() for char in s]))
+    print(any([char.islower() for char in s]))
+    print(any([char.isupper() for char in s]))
