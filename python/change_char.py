58c2dc090e1cd8d03ff57c7a9c2e1e11e3c293c1
Saurabh Bhamra <saurabh.hmr009@gmail.com>
2021-02-23 13:37:55 +0530

    find substring in a string

---
+++ python/change_char.py
@@ -0,0 +1,14 @@
+# Change a character in a string in Python
+
+def mutate_string(string, position, character):
+    string = list(string)
+    string[position] = character
+    string = "".join(string)
+    #string = string[:position]+character+string[position+1:]
+    return string
+
+if __name__ == '__main__':
+    s = input()
+    i, c = input().split()
+    s_new = mutate_string(s, int(i), c)
+    print(s_new)
---
+++ python/find_substring.py
@@ -0,0 +1,16 @@
+# Python program to find the substring in a string. Total number of occurence in the string. 
+
+def count_substring(string, sub_string):
+    count  = 0
+    index = string.find(sub_string)
+    while(index != -1):
+        count += 1
+        index = string.find(sub_string, index+1)
+    return count
+
+if __name__ == '__main__':
+    string = input().strip()
+    sub_string = input().strip()
+    
+    count = count_substring(string, sub_string)
+    print(count)
