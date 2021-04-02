81c5edb88a55272af7f05c8b4d52b30a6e03ebde
Saurabh Bhamra <saurabh.hmr009@gmail.com>
2021-04-02 13:07:10 +0530

    Deletion function is added to the program

--- C/strings/insert_str.c
+++ C/strings/insert_str.c
@@ -23,10 +23,21 @@ void insertion(char *main_str, char *insert_str, int insert_point) {
 	printf("Final string is %s\n", final);
 }
 
+void deletion(char *main_str, int index, int length) {
+	int end = index + length;
+	int i, j = 1;
+
+	for(i = index; i < strlen(main_str); i++, j++) {
+		main_str[i] = main_str[end + j];
+	}
+	main_str[i] = '\0';
+	printf("Final string is %s\n", main_str);
+}
+
 int main(void) {
 	char *main_str = NULL;
 	char *insert_str = NULL;
-	int insert_point;
+	int insert_point, index, length;
 
 	printf("Enter the main string: \n");
 	main_str = (char*)malloc(1024*sizeof(char));
@@ -40,13 +51,19 @@ int main(void) {
 
 	printf("Enter the index from where you want to insert the string: ");
 	scanf("%d", &insert_point);
+	printf("Enter the index from where you want to delete the string: ");
+	scanf("%d", &index);
+	printf("Enter the length of char you want to delete: ");
+	scanf("%d", &length);
+
 
 	if((insert_point == strlen(main_str)-1) || ((insert_point+strlen(insert_str)) > strlen(main_str))) {
 		printf("Either you choose last index as the insertion point or the length of the insertion string is greater than main_str\n");
 		return 1;
 	}
 
-	insertion(main_str, insert_str, insert_point);
+	//insertion(main_str, insert_str, insert_point);
+	deletion(main_str, index, length);
 
 	free(main_str);
 	free(insert_str);
